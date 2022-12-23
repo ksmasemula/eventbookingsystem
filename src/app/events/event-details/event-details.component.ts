467defee1e8001066169963d18a2aafdf98381a9
Kabelo Masemula <ksmasemula@gmail.com>
2022-12-23 17:20:23 +0200

    added create session and list sessions components

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,27 +1,45 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ISession } from '../shared';
 import { EventService } from '../shared/event.service';
 
 @Component({
-  templateUrl:'./event-details.component.html',
-  styles:[`
+  templateUrl: './event-details.component.html',
+  styles: [`
     .container {padding-left:20px; padding-right: 20px;}
-    .event-image {height:100px;}
+    .event-image {height:100px;},
+    a {cursor:pointer;}
   `]
 })
 
-export class EventDetailsComponent implements OnInit{
+export class EventDetailsComponent implements OnInit {
 
-  event:any;
-  eventId?:number;
+
+  event: any;
+  eventId?: number;
+  addMode: boolean = false;
   constructor(
-    private eventService:EventService,
-    private  route:ActivatedRoute
-    ){}
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-      this.eventId = +this.route.snapshot.params['id'];
-      this.event = this.eventService.getEvent(this.eventId);
+    this.eventId = +this.route.snapshot.params['id'];
+    this.event = this.eventService.getEvent(this.eventId);
   }
 
+  addSession() {
+    this.addMode = true;
+  }
+
+  saveNewSession(session: ISession) {
+    session.id = this.event.sessions.length + 1;
+    this.event.sessions.push(session);
+    this.eventService.updateEvent(this.event);
+    this.addMode = false;
+  }
+
+  cancelAddNewSession(){
+    this.addMode = false;
+  }
 }
